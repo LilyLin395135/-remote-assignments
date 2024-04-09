9e9cb731dcbec6928155d8b0b86a42c4db3af6c1
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-09 18:57:51 +0800

    Week-3 Assignment-2 從query string拿數字計算

--- Week-3/Assignments/first-web-server.js
+++ Week-3/Assignments/first-web-server.js
@@ -10,6 +10,29 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });//瀏覽器把請求發送過來後，如果是get請求，且路徑是'/'，則執行這個函數，並且返回html頁面。
 
+//1. user enters http://localhost:3000/data , show a "Lack of Parameter" message
+//2. user enters http://localhost:3000/data?number=xyz, shows a "Wrong Parameter" message
+//3. user enters http://localhost:3000/data?number=5, they should get the result of 1+2+....+5 on the page.
+app.get('/data', (req, res) => {
+    //req包含的客戶端請求屬性：url、params、query、headers、body、method
+    //query用於解析url中的query string，如果URL是/data?number=10，query將包含一個属性number，其值為10。
+    const number = req.query.number; 
+
+    if (number === undefined) { //undefined已宣告但未賦值
+        res.send('Lack of Parameter'); //res.send()發送響應給客戶，會在瀏覽器上看到。
+    }
+    else if (isNaN(number)) {
+        res.send('Wrong Parameter');
+    }
+    else {
+        let sum = 0;
+        for (let i = 1; i <= number; i++) {
+            sum += i;
+        }
+        res.send(`Result: ${sum}`);
+    }
+});
+
 //設定伺服器監聽的port
 app.listen(3000, () => {
     console.log('Server is running at http://localhost:3000 or you can use http://127.0.0.1:3000');
