4e61bebcb8ba41c8221e8c7713ed45b9587ed0ff
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-14 01:03:49 +0800

    style:formating

--- Week-3/Assignment-5/sum-two.js
+++ Week-3/Assignment-5/sum-two.js
@@ -1,20 +1,18 @@
-//Given an array of integers, return indices of the two numbers such that they add up to a specific target. 
-//You may assume that each input would have exactly one solution, and you may not use the same element twice.
 //Two Sum 的演算法是在一個數字陣列中找到兩個數字，使得這兩個數字的和等於一個特定的目標值。
 //1.會先設定一個空的dictionary
 //2.將目標值減去陣列中的數字，拿差值去dictionary中找，找到就回傳index，找不到就把數字存進dictionary。
 //存進dictionary的好處就是可以直接確認有沒有這個key存在，不用再for loop一次。
 //3.找到就return [dictionary[diff], index]
-//Big O: O(n)只要for loop一次就可以找到答案
+//Big O: O(n)，最多只要所有數字做一次就可以找到答案
 
 function twoSum(numbers, target) {
-  const dict = {};
+  const dictionary = {};
   for (let i = 0; i < numbers.length; i++) {
-    const diff = target - numbers[i];
-    if (dict[diff] !== undefined) {
-      return [dict[diff], i];
+    const difference = target - numbers[i];
+    if (dictionary[difference] !== undefined) {
+      return [dictionary[difference], i];
     }
-    dict[numbers[i]] = i;
+    dictionary[numbers[i]] = i;
   }
 }
 
