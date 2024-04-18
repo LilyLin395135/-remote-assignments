24222320c1802ef8d494d4ca506dda858c7ac363
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-18 21:34:15 +0800

    feat:change onreadystatechange to onload

--- Week-4/Assignment-2/ajax-callback-productPage.js
+++ Week-4/Assignment-2/ajax-callback-productPage.js
@@ -1,13 +1,13 @@
 function ajax(src, callback) {
     const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
+    xhr.onload = () => {//onload會在xhr.readyState === 4時執行
+        if (xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);//xhr.responseText是string，要轉成object
             callback(data);
         }
     };
-    xhr.open("GET", src, true);
-    xhr.send();
+    xhr.open("GET", src, true);//第三個參數是async，true是非同步
+    xhr.send();//send request
 }
 
 function render(data) {
@@ -18,13 +18,13 @@ function render(data) {
         name.textContent = product.name;
         const price = document.createElement("p");
         price.textContent = `Price: ${product.price}`;
-        productDiv.appendChild(name);
+        productDiv.appendChild(name);//<div>裡有<h3>和<p>
         productDiv.appendChild(price);
-        container.appendChild(productDiv);
+        container.appendChild(productDiv);//<div>裡有<h3>和<p>，再放到container裡
     });
 }
 
-ajax("https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products", 
-function(response) {
-    render(response);
-});
+ajax("https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products",//products是一個array
+    function (response) {
+        render(response);
+    });
