1fc0c8fcf6ad9f70bf5e8ebf14c4aac85ddfbe44
lilylin395135@gmail.com <lilylin395135@gmail.com>
2024-04-20 16:29:51 +0800

    feat:change commonJS into ES Modules/ put sql in database.js/ use .env

--- Week-4/Assignment-3/app.js
+++ Week-4/Assignment-3/app.js
@@ -1,22 +1,11 @@
-//require方法使用套件Express，放進express變數中，之後都用此變數操作express
-const express = require("express");
-const pool = require("./database");
+//ES Modules方式注入
+import express from "express";
+import { getUsers, getUserById, getUserByEmail, getUserByEmailAndPassword, createUser } from "./database.js";
 
 //啟用一個Express應用程式
 const app = express();
-app.use(express.json()); //使用express.json() middleware 解析請求主體中的JSON資料
+app.use(express.json()); //使用express.json() middleware 解析request.body中的JSON資料
 app.use(express.urlencoded({ extended: true })); //使用express.urlencoded() middleware 解析請求主體中的urlencoded資料
-
-//使用非同步函數，確保資料庫連線成功後，再啟動伺服器
-async function databaseQuery(query, params) {
-  try {
-    const [rows] = await pool.query(query, params);
-    return rows;
-  } catch (error) {
-    console.error("Database query error: ", error);
-    return null; // 回傳 null 或其他標示當查詢失敗時
-  }
-}
 
 //首頁，包含註冊和登入表單
 app.get("/", (request, response) => {
@@ -40,43 +29,31 @@ app.get("/", (request, response) => {
 // 註冊處理
 app.post("/signup", async (request, response) => {
   const { email, password } = request.body;
-  const rows = await databaseQuery("SELECT id FROM user WHERE email = ?", [
-    email,
-  ]);
-  if (!rows) {
-    return response.redirect("/?error=Database+query+failed");
+  //如果email已經存在，回傳email已經註冊過
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return response.redirect("/?error=Email+already+registered");
   }
-  if (rows.length > 0) {
-    response.redirect("/?error=Email+already+registered");
-  } else {
-    await databaseQuery("INSERT INTO user (email, password) VALUES (?, ?)", [
-      email,
-      password,
-    ]);
-    response.redirect("/member");
-  }
+  //email不存在，建立新用戶，並登入member頁面
+  await createUser(email, password);
+  response.redirect("/member");
 });
 
 // 登入處理
 app.post("/signin", async (request, response) => {
   const { email, password } = request.body;
-  const rows = await databaseQuery("SELECT id FROM user WHERE email = ?", [
-    email,
-  ]);
-  if (!rows || rows.length === 0) {
-    // 沒有找到電子郵件，即用戶尚未註冊
-    response.redirect("/?error=Email+not+registered,+please+sign+up");
-  } else {
-    const match = await databaseQuery(
-      "SELECT id FROM user WHERE email = ? AND password = ?",
-      [email, password]
-    );
-    if (match.length > 0) {
-      response.redirect("/member");
-    } else {
-      response.redirect("/?error=Password+incorrect");
-    }
+  //如果email不存在，回傳email尚未註冊，請先註冊
+  const user = await getUserByEmail(email);
+  if (!user) {
+    return response.redirect("/?error=Email+not+registered,+please+sign+up");
   }
+  //如果email存在，但密碼不正確，回傳密碼不正確
+  const validUser = await getUserByEmailAndPassword(email, password);
+  if (!validUser) {
+    return response.redirect("/?error=Password+incorrect");
+  }
+  //email存在且密碼正確，登入member頁面
+  response.redirect("/member");
 });
 
 //會員頁面
--- Week-4/Assignment-3/database.js
+++ Week-4/Assignment-3/database.js
@@ -1,11 +1,67 @@
-const mysql = require("mysql2/promise");
+import mysql from "mysql2";//ES Modules方式注入
 
+import dotenv from "dotenv";//注入.env檔案
+dotenv.config();
+
+
+//【串聯資料庫】
 //pool is a collection and connection to the database
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  user: "lilylin",
-  password: "1qaz@WSX",
-  database: "assignment",
-});
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database:   process.env.MYSQL_DATABASE,
+}).promise();
+//當這個專案用promise而非callback時，要加上promise()，否則會報錯
+//先node database.js測試是否連接成功(Terminal沒有報錯即成功)
 
-module.exports = pool;
+//【操作資料庫】
+//在pool物件上呼叫query方法，就可以執行SQL指令
+//查詢全部user
+export async function getUsers(){
+  const [rows]= await pool.query("select * from user");
+  return rows;
+}
+//用id取user
+export async function getUserById(id){
+  const [rows]= await pool.query(`
+  select * 
+  from user 
+  where id = ?
+  `, [id]);//不能相信的資料要用?，並放id在query的第二個參數，以防使用者傳入會讓資料庫爆掉的資料
+  return rows[0];
+}
+
+//用email取user
+export async function getUserByEmail(email){
+  const [rows]= await pool.query(`
+  select * 
+  from user 
+  where email = ?
+  `, [email]);
+  return rows[0];
+}
+
+//用email和password取user
+export async function getUserByEmailAndPassword(email, password){
+  const [rows]= await pool.query(`
+  select * 
+  from user 
+  where email = ? and password = ?
+  `, [email, password]);
+  return rows[0];
+}
+
+//建立資料，回傳id
+export async function createUser(email, password) {
+  try {
+    const [result] = await pool.query(`
+      INSERT INTO user (email, password)
+      VALUES (?, ?)
+    `, [email, password]);
+    return result.insertId;
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return null; 
+  }
+}
